6daef82fcff6465a4c28ed647386b588720379db
Eng-Fouad <engineer.fouad@hotmail.com>
2016-10-19 02:11:00 -0800

    Add files via upload

--- CalendarWebScraper.java
+++ CalendarWebScraper.java
@@ -38,23 +38,23 @@ public class CalendarWebScraper
 		GREGORIAN_MONTHS_AR_TO_INDEX.put("نوفمبر", 11);
 		GREGORIAN_MONTHS_AR_TO_INDEX.put("ديسمبر", 12);
 		
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الحمل", 1);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الثور", 2);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الجوزاء", 3);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("السرطان", 4);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الأسد", 5);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("السنبلة", 6);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الميزان", 7);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("العقرب", 8);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("القوس", 9);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الجدي", 10);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الدلو", 11);
-		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الحوت", 12);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الميزان", 1);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("العقرب", 2);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("القوس", 3);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الجدي", 4);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الدلو", 5);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الحوت", 6);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الحمل", 7);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الثور", 8);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الجوزاء", 9);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("السرطان", 10);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("الأسد", 11);
+		SOLAR_HIJRI_MONTHS_AR_TO_INDEX.put("السنبلة", 12);
 	}
 	
 	public static void main(String[] args) throws Exception
 	{
-		PrintWriter pw = new PrintWriter(new FileWriter(new File("Hijri-Gregorian-Solar_Hijri-V2.csv")));
+		PrintWriter pw = new PrintWriter(new FileWriter(new File("Hijri-Gregorian-Solar_Hijri-V3.csv")));
 		pw.println("Week Day,Hijri Day,Hijri Month,Hijri Year,Gregorian Day,Gregorian Month,Gregorian Year,Solar Hijri Day,Solar Hijri Month,Solar Hijri Year");
 		
 		String link = "http://www.ummulqura.org.sa/datespage.aspx?d1=true&d2=true&d3=true&d4=true&d5=true&month=%d&year=%d&h=true";
